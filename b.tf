ab91588e3fd64892bd42bd6e30dc3f0dbd7af024
miri_kaufman <mirikaufman4121@gmail.com>
2024-05-05 12:06:11 +0300

    one

---
+++ b.tf
@@ -0,0 +1,9 @@
+resource "aws_instance" "rendered_tags" {
+  ami           = "some-ami"
+  instance_type = "t3.micro"
+
+  tags = merge(var.tags, {
+    yor_name  = "rendered_tags"
+    yor_trace = "50fe3ea3-477a-4275-8d75-9f08aef822ff"
+  })
+}
